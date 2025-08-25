77096b1a03b282456d5918147120e940a6adef8b
jan <zhuojun.jian@litevar.com>
2025-08-25 16:46:17 +0800

    add logger

--- lib/src/controller/controller.dart
+++ lib/src/controller/controller.dart
@@ -3,6 +3,7 @@ import 'package:opentool_daemon/src/utils/utils.dart';
 import 'package:opentool_dart/opentool_dart.dart';
 import 'package:shelf/shelf.dart';
 import '../service/model.dart';
+import '../utils/logger.dart';
 import 'dto.dart';
 import '../service/manage_service.dart';
 import '../service/runner_service.dart';
@@ -18,12 +19,14 @@ class Controller {
 
   Future<Response> getVersion(Request request) async {
     final versionObj = VersionDto(version: version);
+    logger.log(LogModule.http, "getVersionResult", detail: jsonEncode(versionObj.toJson()));
     return Response.ok(jsonEncode(versionObj.toJson()), headers: jsonHeaders,);
   }
 
   Future<Response> list(Request request) async {
     List<OpenToolServerModel> serverModelList= await manageService.list();
     List<OpenToolServerDto> serverDtoList = serverModelList.map((model) => OpenToolServerDto.fromModel(model)).toList();
+    logger.log(LogModule.http, "listResult", detail: jsonEncode(serverDtoList));
     return Response.ok(
       jsonEncode(serverDtoList),
       headers: jsonHeaders,
@@ -36,6 +39,7 @@ class Controller {
 
     try {
       RegisterInfo serverInfo = RegisterInfo.fromJson(data);
+      logger.log(LogModule.http, "register", detail: jsonEncode(serverInfo.toJson()));
       String url = "http://${serverInfo.host}:${serverInfo.port}${serverInfo.prefix}";
       String serverId = generateFixedIdByMd5(url);
       OpenToolServerModel serverModel = OpenToolServerModel(
@@ -51,6 +55,7 @@ class Controller {
       await manageService.add(serverModel);
 
       final result = RegisterResult(id: serverId, error: null,);
+      logger.log(LogModule.http, "registerResult", detail: jsonEncode(result.toJson()));
 
       return Response.ok(jsonEncode(result.toJson()), headers: jsonHeaders,);
     } on FormatException catch (_) {
@@ -66,11 +71,10 @@ class Controller {
 
     try {
       RenameDto renameDto = RenameDto.fromJson(data);
-
+      logger.log(LogModule.http, "rename", detail: jsonEncode(renameDto.toJson()));
       await manageService.rename(renameDto.id, renameDto.name);
-
       StatusDto statusDto = StatusDto(id: renameDto.id, status: StatusType.RENAME_SUCCESS);
-
+      logger.log(LogModule.http, "renameResult", detail: jsonEncode(statusDto.toJson()));
       return Response.ok(jsonEncode(statusDto.toJson()), headers: jsonHeaders,);
     } on FormatException catch (_) {
       return Response.badRequest(body: data);
@@ -85,9 +89,9 @@ class Controller {
 
     try {
       ServerFunctionCallDto functionCallDto = ServerFunctionCallDto.fromJson(data);
-
+      logger.log(LogModule.http, "call", detail: jsonEncode(functionCallDto.toJson()));
       ToolReturn toolReturn = await runnerService.call(functionCallDto.id, functionCallDto.functionCall.toModel());
-
+      logger.log(LogModule.http, "callResult", detail: jsonEncode(toolReturn.toJson()));
       return Response.ok(jsonEncode(ToolReturnDto.fromModel(toolReturn).toJson()), headers: jsonHeaders,);
     } on OpenToolServerNoAccessException catch(e) {
       return Response.ok(jsonEncode(e.toJson()), headers: jsonHeaders,);
@@ -103,11 +107,10 @@ class Controller {
     final data = jsonDecode(payload);
     try {
       ServerIdDto serverIdDto = ServerIdDto.fromJson(data);
-
+      logger.log(LogModule.http, "load", detail: jsonEncode(serverIdDto.toJson()));
       OpenTool? openTool = await runnerService.load(serverIdDto.id);
-
       Map<String, dynamic> result = openTool != null ? openTool.toJson():{};
-
+      logger.log(LogModule.http, "loadResult", detail: jsonEncode(result));
       return Response.ok(jsonEncode(result), headers: jsonHeaders,);
     } on FormatException catch (_) {
       return Response.badRequest(body: data);
@@ -122,9 +125,11 @@ class Controller {
 
     try{
       ServerIdDto serverIdDto = ServerIdDto.fromJson(data);
+      logger.log(LogModule.http, "stop", detail: jsonEncode(serverIdDto.toJson()));
       String serverId = serverIdDto.id;
       await runnerService.stop(serverId);
       StatusDto statusDto = StatusDto(id: serverId, status: StatusType.STOP_SUCCESS);
+      logger.log(LogModule.http, "stopResult", detail: jsonEncode(statusDto.toJson()));
       return Response.ok(jsonEncode(statusDto.toJson()), headers: jsonHeaders);
     } on FormatException catch (_) {
       return Response.badRequest(body: data);
@@ -139,9 +144,11 @@ class Controller {
 
     try{
       ServerIdDto serverIdDto = ServerIdDto.fromJson(data);
+      logger.log(LogModule.http, "remove", detail: jsonEncode(serverIdDto.toJson()));
       String serverId = serverIdDto.id;
       await manageService.remove(serverId);
       StatusDto statusDto = StatusDto(id: serverId, status: StatusType.REMOVE_SUCCESS);
+      logger.log(LogModule.http, "removeResult", detail: jsonEncode(statusDto.toJson()));
       return Response.ok(jsonEncode(statusDto.toJson()), headers: jsonHeaders);
     } on FormatException catch (_) {
       return Response.badRequest(body: data);
---
+++ lib/src/utils/logger.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+import 'package:logging/logging.dart';
+
+enum LogModule { http }
+
+AgentLogger logger = AgentLogger(Level.INFO);
+
+class AgentLogger {
+  static final AgentLogger _singleton = AgentLogger._internal();
+  static Level _level = Level.INFO;
+
+  factory AgentLogger(Level level) {
+    _level = level;
+    return _singleton;
+  }
+
+  Logger? _logger;
+  late File _logFile;
+
+  AgentLogger._internal() {
+    _logger = Logger('OpenToolDaemonLogger');
+    Logger.root.level = _level;
+    _logFile = File('${Directory.current.path}${Platform.pathSeparator}log${Platform.pathSeparator}daemon.log');
+    _logFile.createSync(recursive: true);
+
+    Logger.root.onRecord.listen((record) {
+      final message =
+          '${record.level.name}: ${record.time}: PID $pid: ${record.message}';
+      print(message);
+      _logFile.writeAsStringSync('$message\n', mode: FileMode.append);
+    });
+  }
+
+  void log(LogModule module, String message, {String detail = "{}", Level level = Level.INFO}) {
+    _logger?.log(level, "[${module.name.toUpperCase()}] $message - $detail");
+  }
+}
