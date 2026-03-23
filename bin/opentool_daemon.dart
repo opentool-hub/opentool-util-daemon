fd7687b49284ccf2c984092678303cb41af02eb9
zm <zhiming.liang@litevar.com>
2026-03-23 14:57:14 +0800

    add auto-restore startup parameter

--- bin/opentool_daemon.dart
+++ bin/opentool_daemon.dart
@@ -1,6 +1,13 @@
 import 'package:opentool_daemon/opentool_daemon_server.dart';
+import 'package:opentool_daemon/src/config.dart';
 
-Future<void> main() async {
+Future<void> main(List<String> args) async {
+  if (args.contains('--auto-restore')) {
+    config.autoRestore = true;
+  } else if (args.contains('--no-auto-restore')) {
+    config.autoRestore = false;
+  }
+
   DaemonServer daemonServer = DaemonServer();
   await daemonServer.start();
 }
